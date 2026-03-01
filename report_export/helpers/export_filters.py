62311b2285c775c07f0de986b13cef00c5011612
Muhammad Zaeem Mohzar <zaeem246@gmail.com>
2026-03-01 12:57:22 +0500

    Updates

---
+++ report_export/helpers/export_filters.py
@@ -0,0 +1,71 @@
+from django.db.models import Q
+from datetime import datetime, time
+from django.utils.dateparse import parse_date
+
+
+def export_search_filter(queryset, filters):
+    search = filters.get("search")
+    if search:
+        queryset = queryset.filter(
+            Q(app_slug__icontains=search)
+            | Q(type__icontains=search)
+            | Q(status__icontains=search)
+        )
+    return queryset
+
+
+def filter_export_by_status(queryset, filters):
+    status = filters.get("status")
+    if status:
+        queryset = queryset.filter(status=status)
+    return queryset
+
+
+def filter_export_by_type(queryset, filters):
+    type_ = filters.get("type")
+    if type_:
+        queryset = queryset.filter(type=type_)
+    return queryset
+
+
+def filter_export_by_app_slug(queryset, filters):
+    app_slug = filters.get("app_slug")
+    if app_slug:
+        queryset = queryset.filter(app_slug=app_slug)
+    return queryset
+
+
+def filter_export_by_created_by(queryset, filters):
+    created_by = filters.get("created_by")
+    if created_by:
+        if isinstance(created_by, list):
+            queryset = queryset.filter(created_by__in=created_by)
+        else:
+            queryset = queryset.filter(created_by__in=str(created_by).split(","))
+    return queryset
+
+
+def filter_export_by_date_range(queryset, filters):
+    from_date = filters.get("from_date")
+    to_date = filters.get("to_date")
+    if from_date:
+        queryset = queryset.filter(
+            updated_at__gte=datetime.combine(parse_date(from_date), time.min)
+        )
+    if to_date:
+        queryset = queryset.filter(
+            updated_at__lte=datetime.combine(parse_date(to_date), time.max)
+        )
+    return queryset
+
+
+def filter_by_sort_order(queryset, filters):
+    sort_by = filters.get('sort_by')  # Default sort field
+    sort_order = filters.get('sort_order')
+
+    if sort_by:
+        if sort_order == 'desc':
+            sort_by = f'-{sort_by}'
+        queryset = queryset.order_by(sort_by)
+
+    return queryset
--- report_export/views/reportExportViews.py
+++ report_export/views/reportExportViews.py
@@ -14,33 +14,62 @@ from datetime import datetime, time
 import logging
 from core.utils.date_time_converter import DateTimeConverter
 from rest_framework.decorators import api_view
-
+from ..helpers.export_filters import (
+    export_search_filter,
+    filter_export_by_status,
+    filter_export_by_type,
+    filter_export_by_app_slug,
+    filter_export_by_created_by,
+    filter_export_by_date_range,
+    filter_by_sort_order,
+)   
 logger = logging.getLogger(__name__)
 
-@api_view(['POST'])
-@access_control_middleware
-def report_export_list(request):
-    business_id = request.data.get("auth_business_id")
-    timezone = request.data.get("auth_timezone")
-
+def __get_export_queryset(data, business_id):
     filters = {"business_id": business_id}
-    data = request.data
     is_staff = check_if_user_is_staff(data)
 
     if is_staff == "true":
-        user_id = data.get('auth_id')
-        role_id = data.get('auth_role_id')
+        user_id = data.get("auth_id")
+        role_id = data.get("auth_role_id")
         have_view_all_permission = view_modify_all(user_id, role_id, REPORT_EXPORT_VIEW_ALL)
         queryset = Export.objects.filter(**filters)
         if not have_view_all_permission:
             queryset = queryset.filter(created_by=user_id)
-    else:
-        queryset = Export.objects.filter(**filters)
+        return queryset
+
+    return Export.objects.filter(**filters)
+
+
+def __apply_export_filters(queryset, filters):
+    queryset = export_search_filter(queryset, filters)
+    queryset = filter_export_by_status(queryset, filters)
+    queryset = filter_export_by_type(queryset, filters)
+    queryset = filter_export_by_app_slug(queryset, filters)
+    queryset = filter_export_by_created_by(queryset, filters)
+    queryset = filter_export_by_date_range(queryset, filters)
+    queryset = filter_by_sort_order(queryset, filters)
+    return queryset
+
+
+@api_view(["POST"])
+@access_control_middleware
+def report_export_list(request):
+    data = request.data
+    business_id = data.get("auth_business_id")
+    timezone = data.get("auth_timezone")
 
     last_segment = request.path.rstrip("/").split("/")[-1]
-    filter_data = dict(request.data)
+
+    # normalize list values from request.data (QueryDict behaviour)
+    filter_data = {
+        k: v[0] if isinstance(v, list) and len(v) == 1 else v
+        for k, v in data.items()
+    }
     filter_data["app_slug"] = last_segment
-    queryset = apply_filters(queryset, filter_data).order_by("-created_at")
+
+    queryset = __get_export_queryset(data, business_id)
+    queryset = __apply_export_filters(queryset, filter_data).order_by("-created_at")
 
     paginator = CustomPagination()
     page = paginator.paginate_queryset(queryset, request)
@@ -55,46 +84,6 @@ def report_export_list(request):
         status.HTTP_200_OK,
         paginator.get_paginated_response(result),
     )
-
-
-def apply_filters(queryset, filters):
-    normalized_filters = {}
-    for key, value in filters.items():
-        normalized_filters[key] = value[0] if isinstance(value, list) and len(value) == 1 else value
-    filters = normalized_filters
-
-    search = filters.get("search")
-    if search:
-        queryset = queryset.filter(
-            Q(app_slug__icontains=search)
-            | Q(type__icontains=search)
-            | Q(status__icontains=search)
-        )
-
-    if filters.get("status"):
-        queryset = queryset.filter(status=filters.get("status"))
-
-    if filters.get("type"):
-        queryset = queryset.filter(type=filters.get("type"))
-
-    if filters.get("app_slug"):
-        queryset = queryset.filter(app_slug=filters.get("app_slug"))
-
-    created_by = filters.get("created_by")
-    if created_by:
-        if isinstance(created_by, list):
-            queryset = queryset.filter(created_by__in=created_by)
-        else:
-            queryset = queryset.filter(created_by__in=str(created_by).split(","))
-
-    from_date = filters.get("from_date")
-    to_date = filters.get("to_date")
-    if from_date:
-        queryset = queryset.filter(updated_at__gte=datetime.combine(parse_date(from_date), time.min))
-    if to_date:
-        queryset = queryset.filter(updated_at__lte=datetime.combine(parse_date(to_date), time.max))
-
-    return queryset
 
 
 @api_view(['POST'])
