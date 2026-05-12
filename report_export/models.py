fbb95496acfe9e5bb3833a56d3db000f751d02e7
Muhammad Zaeem Mohzar <zaeem246@gmail.com>
2026-05-13 00:12:12 +0500

    Updates

--- report_export/models.py
+++ report_export/models.py
@@ -9,6 +9,7 @@ class Export(BaseBusinessModel):
     created_by = models.UUIDField(null=True, blank=True)
     payload = models.JSONField(default=dict, blank=True)
     link = models.CharField(max_length=CHAR_LENGTH, null=True, blank=True)
+    file_name = models.CharField(max_length=CHAR_LENGTH, null=True, blank=True)
     class Meta:
         db_table = 'report_export'
         ordering = ['created_at']
