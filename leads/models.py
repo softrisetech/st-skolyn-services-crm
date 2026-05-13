9fc85fc61ffab8e0bfee19c424cade8891e01b97
Hammad Khan <hammad.khan@beaconhousetechnology.com>
2026-05-13 17:51:30 +0500

    fixes

--- leads/models.py
+++ leads/models.py
@@ -249,7 +249,7 @@ class Tracking(BaseBusinessModel):
     model_id = models.UUIDField()
     model_type = models.CharField(max_length=CHAR_LENGTH)
     user_id = models.UUIDField(db_index=True)
-    stage_reason = models.ForeignKey('StageReasonEntry', on_delete=models.CASCADE, db_index=True, null=True, blank=True, related_name='trackings')
+    stage_reason_entry = models.ForeignKey('StageReasonEntry', on_delete=models.CASCADE, db_index=True, null=True, blank=True, related_name='trackings')
 
 
     def get_medium(self):
