305b17463b76a7c82ed2592ac6901a90aa2dc38b
Hammad Khan <hammad.khan@beaconhousetechnology.com>
2026-05-13 18:53:21 +0500

    fixes

--- leads/views/preRequisiteViews.py
+++ leads/views/preRequisiteViews.py
@@ -29,7 +29,7 @@ def get_lead_pre_requisites(request, lead_id):
     timezone = data.get("auth_timezone")
     business_id = data.get('auth_business_id')
 
-    lead = Lead.objects.filter(id=lead_id, business_id=data['business_id']).first()
+    lead = Lead.objects.filter(id=lead_id, business_id=business_id).first()
     if not lead:
         return error_response('lead_not_found', status.HTTP_404_NOT_FOUND)
 
